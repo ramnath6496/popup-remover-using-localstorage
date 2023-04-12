26787dac3bd8e4e41c97cec9ad0e2287a15aa0aa
Ramnath Mandal <107981223+ram-bay20@users.noreply.github.com>
2023-04-12 15:09:44 +0545

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,31 @@
+/* newe js pop popup */
+const removepopup = document.querySelector('#overlayclose');
+const custompopupremove = document.querySelector('.popup-overlay-homepage');
+const popup = document.querySelector('#popup1');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  if (!localStorage.getItem('popupDisplayed')) {
+    // Show the popup
+    removepopup.addEventListener('click', () => {
+      custompopupremove.classList.remove('popup-overlay-homepage');
+      popup.style.display = "none";
+  
+      localStorage.setItem('popupDisplayed', true);
+
+      setTimeout(()=> {
+        localStorage.removeItem('popupDisplayed');
+      }, 10000);
+    });
+  }else{
+    custompopupremove.classList.remove('popup-overlay-homepage');
+    popup.style.display = "none";
+  }
+})
+
+window.addEventListener('scroll', function () {
+  if (custompopupremove.classList.contains('popup-overlay-homepage')) {
+    // Stop scrolling
+    window.scrollTo(0, 0);
+  }
+});
+
